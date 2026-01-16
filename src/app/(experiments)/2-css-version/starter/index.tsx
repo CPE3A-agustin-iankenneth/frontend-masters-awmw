7764a657e2c56603a25199cb358fac980c1be164
Ianszzkieyyy <ian.agustin071729@gmail.com>
2026-01-17 00:10:37 +0800

    completed lesson 2

--- src/app/(experiments)/2-css-version/starter/index.tsx
+++ src/app/(experiments)/2-css-version/starter/index.tsx
@@ -1,11 +1,39 @@
+import styles from "./style.module.css";
+
 export default function Page() {
+ 
+  const text = [
+    "CSS".split(""),
+    "version".split(""),
+  ]
+
   return (
     <div className="bg-blue-300 text-black">
       <div className="flex h-screen items-end justify-left overflow-hidden">
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          CSS
+          {text[0].map((char, i) => {
+            return <span key={i} className={styles.letter}
+            style={
+              {
+                "--index": i,
+              } as React.CSSProperties
+            }
+            >
+              {char}
+            </span>
+          })}
           <br />
-          version
+          {text[1].map((char, i) => {
+            return <span key={i} className={styles.letter}
+            style={
+              {
+                "--index": i,
+              } as React.CSSProperties
+            }
+            >
+              {char}
+            </span>
+          })}
         </h1>
       </div>
     </div>
